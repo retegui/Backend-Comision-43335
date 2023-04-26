16912acf2e0be2ca930967ae57eaa91b613f70cf
retegui <nahuelretegui@gmail.com>
2023-04-26 00:10:16 -0300

    Clases ECMAScript y ECMAScript avanzado

--- main.js
+++ main.js
@@ -1,45 +1,37 @@
 class ProductManager{
     constructor(){
-        this.products=[];
-        
-        // this.title=title;
-        // this.description=description;
-        // this.price=price;
-        // this.thumbnail=thumbnail;
-        // this.code=code;
-        // this.stock=stock;
+        this.products=[];       
     }    
     getProducts=()=>{
         return this.products;
     }
-
     addProduct(title,description,price,thumbnail,code,stock){
         const product={
             title,
             description,
             price,
             thumbnail,
-            code,
             stock,
+            code,
         } 
-
-        if (this.product.length ===0){
-            product.id = 1;
+        if (this.products.length ===0){
+            product.code = 1;
         }
         else{
-            product.id = this.products [this.products.length-1].id+1
+            product.code = this.products [this.products.length-1].id+1
         }
-
     }
-
     getProductById(){
         try {
-            const producto = products.find(elemento=>elemento.id === id);
+            const producto = products.find(elemento=>elemento.code === id);
             return producto;
         } catch (error) {
             return "Not found";
         }
-
     }
-}
+};
 
+const productosStock = new ProductManager();
+productosStock.addProduct("Remera", "Lisa", 8500,)
+
+console.log(productosStock.getProducts());
