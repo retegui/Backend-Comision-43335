de4291cdeb289b6aa1a43e93cf3c8f11757c9481
retegui <nahuelretegui@gmail.com>
2023-05-02 23:51:52 -0300

    Manejo de archivos

--- main.js
+++ main.js
@@ -5,26 +5,25 @@ class ProductManager{
     getProducts=()=>{
         return this.products;
     }
-    addProduct = (title,description,price,thumbnail,code,stock) => {
+    addProduct = (title,description,price,thumbnail,stock) => {
         const product ={
             title,
             description,
             price,
             thumbnail,
-            code,
             stock,
         } 
         if (this.products.length ===0){
-            product.id = 1;
+            product.code = 1;
         }
         else{
-            product.id = this.products [this.products.length-1].id+1
+            product.code = this.products [this.products.length-1].code+1
         }
         this.products.push(product);
     }
-    getProductById(){
+    getProductBycode(){
         try {
-            const producto = products.find(elemento=>elemento.id === id);
+            const producto = products.find(elemento=>elemento.code === code);
             return producto;
         } catch (error) {
             return "Not found";
@@ -37,4 +36,4 @@ productosStock.addProduct("Remera","Lisa",8500,"",175395839,5)
 productosStock.addProduct("Pantalon","negro",15500,"",852837495,4)
 
 console.log(productosStock.getProducts());
-console.log(productosStock.getProductById(3));
+console.log(productosStock.getProductBycode(3));
